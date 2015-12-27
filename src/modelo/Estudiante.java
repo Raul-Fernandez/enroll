69e81b55e01f993a4361273eae0bd45650e31e56
Javier García <jayru1234@gmail.com>
2015-12-27 17:05:12 +0100

    Array[][] para mostrar tabla

--- src/modelo/Estudiante.java
+++ src/modelo/Estudiante.java
@@ -1,6 +1,7 @@
 package modelo;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Enumeration;
 import java.util.Hashtable;
 
@@ -147,6 +148,85 @@ public class Estudiante {
 		}
 		
 		return listado;
+	}
+	
+	public String[][] toTabla(ArrayList<Asignatura> listado){
+		String tabla[][] = new String[6][13];
+		
+		//Llena todas las casillas con ""
+		Arrays.fill(tabla, "");
+		
+		//Inicializamos los días
+		tabla[1][0] = "lunes";
+		tabla[2][0] = "martes";
+		tabla[3][0] = "miércoles";
+		tabla[4][0] = "jueves";
+		tabla[5][0] = "viernes";
+
+		//Inicialuizamos las horas
+		tabla[0][1] = "9";
+		tabla[0][2] = "10";
+		tabla[0][3] = "11";
+		tabla[0][4] = "12";
+		tabla[0][5] = "13";
+		tabla[0][6] = "14";
+		tabla[0][7] = "15";
+		tabla[0][8] = "16";
+		tabla[0][9] = "17";
+		tabla[0][10] = "18";
+		tabla[0][11] = "19";
+		tabla[0][12] = "20";
+		
+		int[] coord;
+		String aux;
+		for(Asignatura a : listado){
+			coord = getCoordenadasTabla(a);
+			aux = tabla[coord[0]][coord[1]];
+			if(aux.length() == 0)
+				tabla[coord[0]][coord[1]] += a.getNombre();
+			else
+				tabla[coord[0]][coord[1]] += " - " + a.getNombre();
+		}
+		return tabla;
+	}
+	
+	/**
+	 * Devuelve la casilla de la tabla en la que tiene que estar la asignatura
+	 * @param a - La asignatura
+	 * @return coord[0] es el día, coord[1] es la hora
+	 */
+	private int[] getCoordenadasTabla(Asignatura a){
+		int coord[] = new int[2];
+		
+		String dia = ""+ a.getDia();
+		int hora = a.getHora();
+		
+		switch (dia.toLowerCase()) {
+			case "l": coord[0] = 1; break;
+			case "m": coord[0] = 2; break;
+			case "x": coord[0] = 3; break;
+			case "j": coord[0] = 4; break;
+			case "v": coord[0] = 5; break;
+			default : break;
+		}		
+		
+		switch (hora) {
+			case 9: coord[1] = 1; break;
+			case 10: coord[1] = 2; break;
+			case 11: coord[1] = 3; break;
+			case 12: coord[1] = 4; break;
+			case 13: coord[1] = 5; break;
+			case 14: coord[1] = 6; break;
+			case 15: coord[1] = 7; break;
+			case 16: coord[1] = 8; break;
+			case 17: coord[1] = 9; break;
+			case 18: coord[1] = 10; break;
+			case 19: coord[1] = 11; break;
+			case 20: coord[1] = 12; break;
+			default : break;
+		}
+		
+		return coord;
 	}
 	
 	/**
